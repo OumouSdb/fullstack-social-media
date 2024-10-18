278798f38c792f35421a9635c1b3dffc281c2af3
Oumou <oumouSdb95@gmail.com>
2024-10-18 12:17:48 +0200

    create dto, model and connecte api to db

---
+++ back/src/main/java/Models/Comments.java
@@ -0,0 +1,23 @@
+package Models;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Comments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+   // private Article articleId;
+  //  private User userId;
+    private LocalDateTime date;
+
+}
