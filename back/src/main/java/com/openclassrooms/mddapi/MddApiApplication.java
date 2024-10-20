e17d9868989878717c657b11bb08bb0820bb38fd
Oumou <oumouSdb95@gmail.com>
2024-10-20 19:43:36 +0200

    fix conflicts

---
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -0,0 +1,21 @@
+package com.openclassrooms.mddapi;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EntityScan(basePackages = "Models")
+public class MddApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MddApiApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}
